d0d2b056118ddb932817b8058658e8e06b2efadc
Baser on Chain <60061293+baseronchain@users.noreply.github.com>
2026-02-17 00:09:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const jitter = (ms, spread = 0.35) => { const d = Math.floor(ms * spread); const
 function askQuestion(query) {
     const rl = readline.createInterface({
         input: process.stdin,
-        output: process.stdout,
+        output: process.stdout, 
     });
 
     return new Promise(resolve => rl.question(query, ans => {
